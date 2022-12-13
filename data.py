49b0e5e75e8d9f9a15f2aa23abd5419428316b3a
LudovicR <ludo1093@gmail.com>
2022-12-13 13:56:39 -0500

    Retourne Tensor de bonne dimension dans [0,1]

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import h5py
 import numpy as np
 
 from torch.utils.data import Dataset
+import torchvision.transforms as T
 
 class GalaxiesDataset(Dataset):
     def __init__(self, path):
@@ -9,9 +10,12 @@ class GalaxiesDataset(Dataset):
         with h5py.File(path, 'r') as F:
             self.images = np.array(F['images'])
             self.labels = np.array(F['ans'])
+        # Définit les opérations de preprocessing
+        # self.transform = T.Compose([T.ToTensor(), T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        self.transform = T.ToTensor()
 
     def __getitem__(self, index):
-        image = self.images[index]
+        image = self.transform(self.images[index])
         label = self.labels[index]
         return image, label
 
